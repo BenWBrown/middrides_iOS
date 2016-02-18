9b7bb2051473ce9d9b0856c074d2b13714b42b59
Ben Brown <Ben@bens-mbp-6.middlebury.edu>
2016-02-17 20:51:12 -0500

    fix Connectivity linking issue

--- source_files/middrides/Connectivity.swift
+++ source_files/middrides/Connectivity.swift
@@ -2,33 +2,8 @@
 //  Connectivity.swift
 //  middrides
 //
-//  Created by Sherif Nada on 2/16/16.
+//  Created by Ben Brown on 2/17/16.
 //  Copyright © 2016 Ben Brown. All rights reserved.
 //
 
-/**
-    Utility class to check if internet connection is active
-*/
-import SystemConfiguration
-
-public class Connectivity {
-    class func isConnectedToNetwork() -> Bool {
-        
-        var zeroAddress = sockaddr_in();
-        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress));
-        zeroAddress.sin_family = sa_family_t(AF_INET);
-        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
-            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
-        }
-        
-        var flags = SCNetworkReachabilityFlags()
-        
-        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
-            return false
-        }
-        
-        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0 ;
-        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0 ;
-        return (isReachable && !needsConnection)
-    }
-}
+import Foundation
